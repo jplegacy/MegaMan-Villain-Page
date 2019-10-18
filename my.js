55713f986212eede42a9518863532401a6b5fedb
Web 2021 <web2021@301-3-6.local>
2019-10-18 14:16:31 -0400

    First Commit

---
+++ my.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+                $(".scene1").hide(0);
+                $(".webpage").hide(0);
+                $(".readyScreen").hide(0);
+
+                $("body").css("background-color","black");
+ 
+                
+                $(".start").click(function(){  
+                        $(".starting").fadeOut(400);
+                        $(".scene1").delay(400).fadeIn(400);
+                        $(".scene1").delay(3000).fadeOut(400);
+                        $(".readyScreen").delay(4500).fadeIn(400);
+                        $("body").css("background-color","#36538b");
+                    });
+    
+});
